b904e202e7d32e71ad7c22ccc77d36b181478342
Frank <franklinjalves@gmail.com>
2025-10-28 22:29:07 +0000

    fix: zindex

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -60,7 +60,7 @@ export default function ChatWidget() {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-[9999] font-sans">
+    <div className="fixed bottom-6 right-6 z-[999999999] font-sans">
       {!isOpen && (
         <Button
           onClick={() => setIsOpen(true)}
